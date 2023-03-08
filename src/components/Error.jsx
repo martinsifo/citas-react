7aa3eed085f4d0f14d40f20ad2d018c2fc8ffbb8
martinsifo <sifomartinm@gmail.com>
2023-03-08 01:17:59 -0300

    style: format files

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,9 @@
-function Error({children}) {
+function Error({ children }) {
   return (
-    <div className='bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md'>
-    <p>{children}</p>
+    <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+      <p>{children}</p>
     </div>
-  )
+  );
 }
 
-export default Error
+export default Error;
